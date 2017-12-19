478784d66dc0556419829fb22684b0ebad599cdc
Matt Haws <matthaws@gmail.com>
2017-12-19 11:08:28 -0500

    Finish day seventeen part 2

--- seventeen.rb
+++ seventeen.rb
@@ -1,21 +1,20 @@
 def spinlock
   count = 0
   idx = 0
-  buffer = [0]
+  buffer_length = 1
   steps = 348
+  highest = 0
 
   until count == 50000000
     count += 1
-    idx = 348 % buffer.length + idx
-    idx = idx % buffer.length if idx > buffer.length
-    front = buffer[0..idx] || []
-    back = buffer[idx+1..-1] || []
-    buffer = front + [count] + back
+    idx = (348 + idx) % buffer_length
     idx += 1
+    buffer_length += 1
+    highest = count if idx == 1
   end
-
+  p buffer_length
   # buffer[buffer.index(2017) + 1]
-  buffer[buffer.index(0) + 1]
+  highest
 end
 
 p spinlock
